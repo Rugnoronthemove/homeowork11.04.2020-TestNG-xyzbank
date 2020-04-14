ddde713ea02dbff267292a72f3aef2e0c30090b0
Rugnoronthemove <rugnor.onthemove@gmail.com>
2020-04-14 12:37:15 +0100

    homework 11.04.2020 submission

---
+++ src/test/java/com/bank/testbase/TestBase.java
@@ -0,0 +1,35 @@
+package com.bank.testbase;
+
+import com.bank.basepage.BasePage;
+import com.bank.browserselector.BrowserSelector;
+import com.bank.loadproperty.LoadProperty;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase extends BasePage {
+
+    //object creation
+    BrowserSelector browserSelector = new BrowserSelector();
+    LoadProperty loadProperty = new LoadProperty();
+
+    //project Url
+    String browser = loadProperty.getProperty("browser");
+    String baseUrl = loadProperty.getProperty("baseUrl");
+
+    //opens browser
+    @BeforeTest(groups = {"Regression","Smoke","Sanity"})
+    public void openBrowser() {
+        browserSelector.selectBrowser(browser);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+    }
+
+    //close browser
+    @AfterTest(groups = {"Regression","Smoke","Sanity"})
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
